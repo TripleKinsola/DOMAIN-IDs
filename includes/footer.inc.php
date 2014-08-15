1ea5218897b419b5184a3e0026450a03243e1603
TripKinsola <TripleKinsola@gmail.com>
2014-08-15 17:05:18 +0100

    footer adjusted

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,5 @@
 	<footer>
-		&copy All Right Reserved at my Mummy's kitchen, <?php echo date("Y"); ?>.
+		&copy All Right Reserved at<a href="http://geekerbyte.blogspot.com" target="_blank"> my Mummy's kitchen</a>, <?php echo date("Y"); ?>.
 	</footer>>
 </body>
 </html>
